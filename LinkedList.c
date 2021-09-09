331066c031d61d3808802996b454b181ff10546d
Anubhav Singh <42691802+anubha2012v@users.noreply.github.com>
2021-09-09 08:54:56 +0530

    Update LinkedList.c

--- LinkedList.c
+++ LinkedList.c
@@ -225,11 +225,12 @@ int main()
 //     Insert(first, 0, 5);
     
     
-    InsertLast(10);
-    InsertLast(12);
-    InsertLast(14);
-    InsertLast(16);
-    InsertLast(18);
-    Display(first);
+//     InsertLast(10);
+//     InsertLast(12);
+//     InsertLast(14);
+//     InsertLast(16);
+//     InsertLast(18);
+//     Display(first);
+    
     return 0;
 }
