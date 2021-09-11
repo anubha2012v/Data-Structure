47eac84bb9b30e0bd29d88fe0c475d5bac26563e
Anubhav Singh <42691802+anubha2012v@users.noreply.github.com>
2021-09-11 10:22:32 +0530

    Reversing the Linked List

--- LinkedList.c
+++ LinkedList.c
@@ -288,6 +288,27 @@ void RemoveDuplicate(struct Node *p){
     }
 } 
 
+//Reversing in linked list
+void Reverse1(struct Node *p){
+    int *A, i=0;
+    struct Node *q = p;
+
+    A = (int *)malloc(sizeof(int)* count(p));
+
+    while(q!=NULL){
+        A[i] = q->data;
+        q = q->next;
+        i++;
+    }
+    q = p;
+    i--;
+    while(q != NULL){
+        q->data = A[i];
+        q = q->next;
+        i--;
+    }
+}
+
 
 int main()
 {
