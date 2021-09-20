7eaa828af3eac2956db8d6f71d1f37fc12c3aaaf
Anubhav Singh <42691802+anubha2012v@users.noreply.github.com>
2021-09-20 09:28:23 +0530

    Update StackUsingLinkedList.c

--- StackUsingLinkedList.c
+++ StackUsingLinkedList.c
@@ -1,2 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+
+int main()
+{
+    push(10);
+    push(20);
+    push(30);
+    push(40);
+
+    Display();
+
+    printf("%d ", isFull());
+    return 0;
+}
