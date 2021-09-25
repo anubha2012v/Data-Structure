72b2e2255bc5c1e9b47207d4d915250ecfaecbe7
Anubhav Singh <42691802+anubha2012v@users.noreply.github.com>
2021-09-25 10:24:27 +0530

    Reversing Using String Buffer in JAVA

---
+++ String/ReverseString.java
@@ -0,0 +1,8 @@
+class ReverseString{
+    public static void main(String[] args) {
+        String str = "Anubhav Singh";
+        StringBuffer sbr = new StringBuffer(str);
+        sbr.reverse();
+        System.out.println(sbr);
+    }
+}
