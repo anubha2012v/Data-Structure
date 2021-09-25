b4337d3bf8cbfdd2048143dc432f1478a8ab69ee
anubha2012v <singh.anubhav2012@gmail.com>
2021-09-25 07:38:58 +0530

    String Operation.

---
+++ String/ReversingString.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    char A[] = "Anubhav";
+    char B[7];
+    int i,j;
+
+    for(i=0; A[i] != '\0'; i++){}
+    i = i-1;
+    for(j = 0; i >=0; i--,j++) {
+        B[j] = A[i];
+    }
+    B[j] = '\0';
+    printf("%s \n",B);
+
+    return 0;
+}
