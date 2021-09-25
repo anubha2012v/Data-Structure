c0a91fb630dfccbfd4e4e168e3288fffd977ef36
Anubhav Singh <42691802+anubha2012v@users.noreply.github.com>
2021-09-25 10:40:56 +0530

    Length of a String Using JAVA

---
+++ String/LengthString.java
@@ -0,0 +1,7 @@
+public class LengthString {
+    public static void main(String[] args) {
+        String str = "Anubhav Singh";
+        int length = str.length();
+        System.out.println(length);
+    }
+}
